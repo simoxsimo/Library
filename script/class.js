b50a929a7a1b78068f7f0e02fddb84bbbddc575a
simoxsimo <touili.mohammed@gmail.com>
2020-05-05 15:46:23 +0000

    Fixed linter error

--- script/class.js
+++ script/class.js
@@ -7,6 +7,7 @@ export default function Book(author, title, pages, read) {
 }
 
 // Toggle read status
+/* eslint func-names: ["error", "never"] */
 Book.prototype.changeReadStatus = function () {
   if (this.read === 'Yes') {
     this.read = 'No';
